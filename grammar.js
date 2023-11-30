de4908f50ae43b04e085b8d024eb0fedc5b6ead2
wstevick <12481632w@gmail.com>
2023-11-30 15:30:19 -0500

    format grammar.js

--- grammar.js
+++ grammar.js
@@ -1,51 +1,65 @@
-module.exports = grammar({
-    name: 'futhark',
+export default grammar({
+  name: "futhark",
 
-    extras: ($, original) => [...original, $.comment],
+  extras: ($, original) => [...original, $.comment],
 
-    word: $ => $.name,
+  word: ($) => $.name,
 
-    precedences: $ => [
-        ['neg', 'mult', 'add', 'if'],
-    ],
+  precedences: () => [
+    ["neg", "mult", "add", "if"],
+  ],
 
-    rules: {
-        source_file: $ => repeat($._dec),
-        
-        _dec: $ => choice($.val_bind),
+  rules: {
+    source_file: ($) => repeat($._dec),
 
-        val_bind: $ => seq('def', field('bindto', $.name), '=', field('val', $._exp)),
+    _dec: ($) => choice($.val_bind),
 
-        _atom: $ => choice($.name, seq('(', $._exp, ')')),
-        _exp: $ => choice($._atom, $.apply, $.binary, $.neg, $.if),
+    val_bind: ($) => seq("def", field("bindto", $.name), "=", field("val", $._exp)),
 
-        apply: $ => seq(field('func', $._atom), field('args', repeat1($._atom))),
+    _atom: ($) => choice($.name, seq("(", $._exp, ")")),
+    _exp: ($) => choice($._atom, $.apply, $.binary, $.neg, $.if),
 
-        binary: $ => {
-            // a "level" represents a group of operators with the same precedence
-            const levels = [
-                ['left', 'add', ['+', '-']],
-                ['left', 'mult', ['*', '/']],
-            ];
-            const rules = levels.map(level => {
-                const assoc = level[0];
-                const precName = level[1];
-                const ops = level[2];
-                return prec[assoc](precName,
-                    seq(field('lhs', $._exp),
-                        field('op', alias(choice(...ops), $.symbol)),
-                        field('rhs', $._exp)));
-            });
-            return choice(...rules);
-        },
+    apply: ($) => seq(field("func", $._atom), field("args", repeat1($._atom))),
 
-        neg: $ => prec('neg', seq('-', field('negated', $._exp))),
-
-        if: $ => prec('if', seq('if', field('cond', $._exp), 'then', field('then', $._exp), 'else', field('else', $._exp))),
-
-        name: $ => /[a-zA-Z_][a-zA-Z0-9_']*/,
-        symbol: $ => /[+\-*\/%=!><|%^][+\-*\/%=!><|%^.]*/,
-
-        comment: $ => /--.*\n/,
+    binary: ($) => {
+      // a "level" represents a group of operators with the same precedence
+      const levels = [
+        ["left", "add", ["+", "-"]],
+        ["left", "mult", ["*", "/"]],
+      ];
+      const rules = levels.map((level) => {
+        const assoc = level[0];
+        const precName = level[1];
+        const ops = level[2];
+        return prec[assoc](
+          precName,
+          seq(
+            field("lhs", $._exp),
+            field("op", alias(choice(...ops), $.symbol)),
+            field("rhs", $._exp)
+          )
+        );
+      });
+      return choice(...rules);
     },
+
+    neg: ($) => prec("neg", seq("-", field("negated", $._exp))),
+
+    if: ($) => prec(
+      "if",
+      seq(
+        "if",
+        field("cond", $._exp),
+        "then",
+        field("then", $._exp),
+        "else",
+        field("else", $._exp)
+      )
+    ),
+
+    name: () => /[a-zA-Z_][a-zA-Z0-9_']*/,
+    symbol: () => /[+\-*\/%=!><|%^][+\-*\/%=!><|%^.]*/,
+
+    comment: () => /--.*\n/,
+  },
 });
